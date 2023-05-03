b3601536894f6be87d318de735b5f6801b159524
Sanmitha <sanmithasadanand@gmail.com>
2023-05-04 01:53:20 +0530

    Add onmouseover styling to the grid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const containerDiv = document.querySelector(".container")
-const gridDiv = document.querySelectorAll(".grid");
+
 console.log(containerDiv);
 //Create Grid
 function createGrid(rows, cols){
@@ -8,12 +8,19 @@ function createGrid(rows, cols){
     containerDiv.style.setProperty('--grid-cols', cols);
     for (c =0; c< (rows*cols) ; c++ ){
         let cell = document.createElement('div');
-        /* cell.innerText = (c + 1); */
         cell.classList.add("new-div");
         containerDiv.appendChild(cell);
-/*         cell.style.width="600/rows"
-        cell.style.height= "600/rows" */
     };
-};
+}; 
 //Default Grid:
 createGrid(16,16);
+const item = document.querySelectorAll('.new-div');
+  item.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.background = 'black';
+    });
+  });
+
+
+
+ /* let rows = prompt('How many Rows or Columns do you need?'); */
